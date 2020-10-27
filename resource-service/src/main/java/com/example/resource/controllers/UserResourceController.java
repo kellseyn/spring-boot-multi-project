5e0bca65bf4d88c2ebfe4d7d6f89380a4e9a16bd
Kellsey Neagley <kellseyneagley@gmail.com>
2020-10-26 20:57:14 -0500

    added feign and endpoints to get endpoints from the user crud

---
+++ resource-service/src/main/java/com/example/resource/controllers/UserResourceController.java
@@ -0,0 +1,32 @@
+package com.example.resource.controllers;
+
+import com.example.resource.dtos.UserDetailsDTO;
+import com.example.resource.clients.UserCrudFeign;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class UserResourceController {
+
+    @Autowired
+    private UserCrudFeign userCrudFeign;
+
+    @PostMapping("/save-user")
+    public UserDetailsDTO saveUpdate(@RequestBody UserDetailsDTO inputUser){
+        return userCrudFeign.callUserCrudToSaveUser(inputUser);
+    }
+
+    @GetMapping("/getById/{id}")
+    public UserDetailsDTO getUserById(@PathVariable Long id){
+        return userCrudFeign.callUserCrudToGetById(id);
+    }
+
+    @GetMapping("/getByName/{name}")
+    public List<UserDetailsDTO> getUsersByName(@PathVariable String name){
+        return userCrudFeign.callUserCrudToGetUsersByName(name);
+    }
+
+}
